0f5b7c3aad9c35ea3e5a335f94e20301403f8704
mzamor <zamoramariano@hotmail.com>
2021-11-16 11:21:34 -0300

    drawing on the map

--- app/src/main/java/com/example/runningapp/services/TrackingService.kt
+++ app/src/main/java/com/example/runningapp/services/TrackingService.kt
@@ -74,10 +74,12 @@ class TrackingService  : LifecycleService(){
                         isFirstRun = false
                     }else{
                         Timber.d("Resuming service")
+                        startForegroundService()
                     }
                 }
                 ACTION_PAUSE_SERVICE ->{
                     Timber.d("Paused service")
+                    pauseService()
                 }
                 ACTION_STOP_SERVICE ->{
                     Timber.d("Stop service")
@@ -86,6 +88,10 @@ class TrackingService  : LifecycleService(){
         }
 
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun pauseService(){
+        isTracking.postValue(false)
     }
 
     @SuppressLint("MissingPermission")
--- app/src/main/java/com/example/runningapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/runningapp/ui/viewmodels/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import com.example.runningapp.repositories.MainRepository
 
+
 class MainViewModel @ViewModelInject constructor(
     val mainRepository : MainRepository
 ): ViewModel() {
